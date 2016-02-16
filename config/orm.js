2960a15eaf36dbf321aebfcc119605cbae7a705a
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-02-16 15:42:08 -0500

    ran into an issues with my key pairings in the orm, going to rework it

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,33 @@
 var connection = require('../config/connection.js');
 
-exports = {
 
-var orm = {
+  var orm = {
 
-var showAll : function(b){
-  var currentTableData = 'SELECT * FROM burger_table;';
-  connection.query(currentTableData, function(err,result){
-    if (err) throw err;
-    b(result);
-  });
-},
+    var showAll: function(b){
+      var currentTableData = 'SELECT * FROM burger_table;';
+      connection.query(currentTableData, function(err,result){
+        if (err) throw err;
+        b(result);
+      });
+    },
 
- var addsBurger: function(a,b) {  
-      var x = 'INSERT INTO burger_table (burger_name, devoured) VALUES (?, ?);';
-      connection.query(x, [a, false], function(err, result) {
-      if (err) throw err;
-      b(result);
-  });
-},
+     var addsBurger: function(a,b) {  
+          var x = 'INSERT INTO burger_table (burger_name, devoured) VALUES (?, ?);';
+          connection.query(x, [a, false], function(err, result) {
+          if (err) throw err;
+          b(result);
+      });
+    },
 
- var devoured : function(a,b){
-  var x = 'UPDATE burger_table SET devoured=true WHERE burger_name=?';
-  connection.query(x, [a]), function(err, result){
-    if (err) throw err;
-    b(result);
-  });
-  }
-}
+     var devoured: function(a,b){
+      var x = 'UPDATE burger_table SET devoured=true WHERE burger_name=?';
+      connection.query(x, [a]), function(err, result){
+        if (err) throw err;
+        b(result);
+      });
+    }
+  } 
 
-    
 
 module.exports = orm;
  
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,34 +1,13 @@
 var burger = require('../models/burger.js');
 var express = require('express');
-var router = require('router');
-var y = express();
+var router = express.Router();
 
-
-module.exports.burgers_controller = function(y){
-  y.get('/', function(req,res){
-    var data = {
-      burger.showAll(function(z){
-        var data = {
-          showALL : z
-        }
-        res.render('index',{z});
-      });
-    });
-  y.post('/new',function(req,res){
-    burger.addBurger(req.body.burger_name, function(result){
-      res.redirect('/');
-    });
-  });
-
-y.post('/eaten/:burger_name'), function(req,res){
-  burger.devour(req.params.burger_name,function(result){
-    res.redirect('/');
+router.get('/', function(req,res) {
+  burger.showAll(function(burger_data){
+    res.render('index', {burger_data});
   });
 });
- 
-  
-}
 
-router.get('/index'), function(req,res){
-theBurgers.showAll(function)
-}
+
+
+module.exports = router;
